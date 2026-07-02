6f03036ee5b90087de093c30f134000da4d7ebcc
Lucas <lucas@yesbabylon.com>
2026-07-02 14:02:26 +0200

    add handle quota reached controllers

---
+++ infra/actions/quota/handle-edms-storage-size-reached.php
@@ -0,0 +1,39 @@
+<?php
+/*
+    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
+    Some Rights Reserved, Yesbabylon SRL, 2020-2026
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+use infra\quota\QuotaUsage;
+
+[$params, $providers] = eQual::announce([
+    'description'   => "Handles quota reached for 'edms.storage.size'.",
+    'params'        => [
+    ],
+    'response'      => [
+        'content-type'  => 'application/json',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'providers'     => ['context']
+]);
+
+/**
+ * @var \equal\php\Context $context
+ */
+['context' => $context] = $providers;
+
+$quota_usage = QuotaUsage::search(['code', '=', 'edms.storage.size'])
+    ->read(['id'])
+    ->first();
+
+if(!$quota_usage) {
+    throw new Exception('unknown_quota_usage', EQ_ERROR_INVALID_CONFIG);
+}
+
+QuotaUsage::id($quota_usage['id'])->update(['is_reached' => true]);
+
+$context
+    ->httpResponse()
+    ->send();
